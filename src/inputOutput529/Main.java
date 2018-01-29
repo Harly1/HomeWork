9e55f0866384865f12bf30a63de7a23d7b24e011
harly <harly1@yandex.ru>
2018-01-29 18:05:26 +0300

    Добавил тест

--- src/inputOutput529/Main.java
+++ src/inputOutput529/Main.java
@@ -6,11 +6,41 @@ import java.io.InputStreamReader;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String str = reader.readLine();
 
+
+ byte[] bytes = {65, 66, 13, 13, 10, 10, 13, 67, 13, 13};
+//            byte[] expectedBytes = { 65, 66, 13,     10, 10, 13, 67, 13, 13 };
+//            byte[] bytes =         { 65, 13, 10, 10, 13 };
+//            byte[] expectedBytes = { 65,     10, 10, 13 };
+//            byte[] bytes =         { 1, 2, 13, 10, 3, 4, 13, 10, 5, 6 };
+//            byte[] expectedBytes = { 1, 2,     10, 3, 4,     10, 5, 6 };
+//            byte[] bytes =         { 1, 2, 3, 13, 10, 13, 10, 4, 5, 6 };
+//            byte[] expectedBytes = { 1, 2, 3,     10,     10, 4, 5, 6 };
+//            byte[] bytes =         { 1, 2, 3 };
+//            byte[] expectedBytes = { 1, 2, 3 };
+//            byte[] bytes =         { 13, 10, 1, 2, 3, 4, 5, 6 };
+//            byte[] expectedBytes = {     10, 1, 2, 3, 4, 5, 6 };
+//            byte[] bytes =         { 1, 2, 3, 13, 10, 4, 5, 6 };
+//            byte[] expectedBytes = { 1, 2, 3,     10, 4, 5, 6 };
+//            byte[] bytes =         { 1, 2, 3, 4, 5, 6, 13, 10 };
+//            byte[] expectedBytes = { 1, 2, 3, 4, 5, 6,     10 };
+//            byte[] bytes =         { 1, 2, 3, 13, 13, 10, 4, 5, 6 };
+//            byte[] expectedBytes = { 1, 2, 3, 13,     10, 4, 5, 6 };
+//            byte[] bytes =         { 1, 2, 3, 10, 13, 4, 5, 6 };
+//            byte[] expectedBytes = { 1, 2, 3, 10, 13, 4, 5, 6 };
+//          byte[] bytes =  {};
+
+     InputStreamReader reader = new  InputStreamReader(System.in);
+     int input = System.in.read(bytes);
             while(reader.ready()){
                 int piece = reader.read();
+                if(piece!=13){
+                    piece = reader.read();
+                    if(piece!=10){
+                        System.out.println(piece);
+                    }
+                }
+
                 System.out.println(piece);
             }
 
