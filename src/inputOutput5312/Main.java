5e5209c3c634fdbb2bb19b9f67a5a87e40cc997e
harly <harly1@yandex.ru>
2018-01-31 18:15:50 +0300

    Задача 5312

---
+++ src/inputOutput5312/Main.java
@@ -0,0 +1,36 @@
+package inputOutput5312;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        InputStream inputStream = new ByteArrayInputStream(new byte[]{48, 49, 50, 51});
+        System.out.println(readAsString(inputStream,Charset.forName("ASCII")));
+
+    }
+
+    public static String readAsString(InputStream inputStream, Charset charset) throws IOException {
+        inputStream = null;
+        StringBuilder str = new StringBuilder();
+        try(InputStreamReader inputStreamReader = new InputStreamReader(inputStream,charset)){
+
+            while (inputStreamReader.ready()){
+                char cur = (char) inputStreamReader.read();
+                str.append(cur);
+            }
+
+            return str.toString();
+
+        } catch (Throwable e){
+            return str.toString();
+
+        }
+
+
+    }
+
+}
