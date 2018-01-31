912ad844e0afa568ba3bca78b69e1ebb586e9f85
harly <harly1@yandex.ru>
2018-01-31 16:58:47 +0300

    Задача 529 и FizzBuzz

---
+++ src/FizzBuzz.java
@@ -0,0 +1,26 @@
+public class FizzBuzz {
+    public static void main(String[] args) {
+        for (int i = 1; i <11 ; i++) {
+
+            if((i % 2 == 0) && (i % 3 == 0) ) {
+                System.out.println("FizzBuzz");
+            } else
+
+            if(i % 2 == 0){
+                System.out.println("Fizz");
+
+            } else
+
+            if(i % 3 == 0){
+                System.out.println("Buzz");
+            }
+
+
+        }
+        for (int i = 1; i <11 ; i++) {
+            System.out.println(i);
+
+
+        }
+    }
+}
--- src/inputOutput529/Main.java
+++ src/inputOutput529/Main.java
@@ -1,50 +1,61 @@
 package inputOutput529;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+
 
 public class Main {
     public static void main(String[] args) throws IOException {
 
 
-              byte[] bytes = {65, 66, 13, 13, 10, 10, 13, 67, 13, 13};
+//              byte[] bytes = {65, 66, 13, 13, 10, 10, 13, 67, 13, 13};
 //            byte[] expectedBytes = { 65, 66, 13,     10, 10, 13, 67, 13, 13 };
 //            byte[] bytes =         { 65, 13, 10, 10, 13 };
 //            byte[] expectedBytes = { 65,     10, 10, 13 };
-//            byte[] bytes =         { 1, 2, 13, 10, 3, 4, 13, 10, 5, 6 };
+//            byte[] bytes = { 1, 2, 13, 10, 3, 4, 13, 10, 5, 6 };
 //            byte[] expectedBytes = { 1, 2,     10, 3, 4,     10, 5, 6 };
-//            byte[] bytes =         { 1, 2, 3, 13, 10, 13, 10, 4, 5, 6 };
+//            byte[] bytes ={ 1, 2, 3, 13, 10, 13, 10, 4, 5, 6 };
 //            byte[] expectedBytes = { 1, 2, 3,     10,     10, 4, 5, 6 };
-//            byte[] bytes =         { 1, 2, 3 };
+//            byte[] bytes = { 1, 2, 3 };
 //            byte[] expectedBytes = { 1, 2, 3 };
-//            byte[] bytes =         { 13, 10, 1, 2, 3, 4, 5, 6 };
+//            byte[] bytes = { 13, 10, 1, 2, 3, 4, 5, 6 };
 //            byte[] expectedBytes = {     10, 1, 2, 3, 4, 5, 6 };
-//            byte[] bytes =         { 1, 2, 3, 13, 10, 4, 5, 6 };
+//            byte[] bytes = { 1, 2, 3, 13, 10, 4, 5, 6 };
 //            byte[] expectedBytes = { 1, 2, 3,     10, 4, 5, 6 };
-//            byte[] bytes =         { 1, 2, 3, 4, 5, 6, 13, 10 };
+//            byte[] bytes = { 1, 2, 3, 4, 5, 6, 13, 10 };
 //            byte[] expectedBytes = { 1, 2, 3, 4, 5, 6,     10 };
-//            byte[] bytes =         { 1, 2, 3, 13, 13, 10, 4, 5, 6 };
+//            byte[] bytes = { 1, 2, 3, 13, 13, 10, 4, 5, 6 };
 //            byte[] expectedBytes = { 1, 2, 3, 13,     10, 4, 5, 6 };
-//            byte[] bytes =         { 1, 2, 3, 10, 13, 4, 5, 6 };
+//            byte[] bytes = { 1, 2, 3, 10, 13, 4, 5, 6 };
 //            byte[] expectedBytes = { 1, 2, 3, 10, 13, 4, 5, 6 };
-//           byte[] bytes =  {};
-
-     InputStreamReader reader = new  InputStreamReader(System.in);
-//     InputStreamReader writer = new  InputStreamReader(System.out.write());
-            int piece = reader.read();
-            while(piece != -1){
-                if(piece!=13){
-                    if(piece!=10){
-                        System.out.write(piece);
-
-                    }
-                }
+//              byte[] bytes =  {};
 
 
-                piece = reader.read();
+
+//        InputStream inputStream = new ByteArrayInputStream(bytes);
+        InputStream inputStream = System.in;
+
+
+        int cur = inputStream.read();
+        int next = inputStream.read();
+        while(cur != -1){
+
+            if ((next == 10) && (cur == 13)) {
+
+
+            } else {
+//                    System.out.print(cur + " ");
+                System.out.write(cur);
             }
+
+            cur = next;
+            next = inputStream.read();
+        }
+
+        System.out.flush();
 
     }
 
 }
+
+
+
