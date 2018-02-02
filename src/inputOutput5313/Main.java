a4cc9e359cdbc2c7da4794ae9493ee0bf9d4e1cd
harly <harly1@yandex.ru>
2018-02-02 17:48:07 +0300

    Задача 5313

---
+++ src/inputOutput5313/Main.java
@@ -0,0 +1,11 @@
+package inputOutput5313;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.nio.file.Files;
+
+public class Main {
+    public static void main(String[] args) {
+        BufferedReader reader = Files.newBufferedReader();
+    }
+}
