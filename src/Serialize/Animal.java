4a46c92e30d31096879bc009bbd412c01d46d349
harly <harly1@yandex.ru>
2018-02-08 11:52:13 +0300

    Задача про сериализацию

--- src/Serialize/Animal.java
+++ src/Serialize/Animal.java
@@ -1,6 +1,6 @@
 package Serialize;
 
-import java.io.Serializable;
+import java.io.*;
 import java.util.Objects;
 
 class Animal implements Serializable {
@@ -10,6 +10,36 @@ class Animal implements Serializable {
         this.name = name;
     }
 
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        deserializeAnimalArray(Test());
+     Test();
+
+    }
+    public static byte[] Test() throws IOException {
+
+        int size = 3;
+
+        Animal dog = new Animal("пес");
+        Animal cat = new Animal("кот");
+        Animal lemur = new Animal("лемур");
+        Animal[] animalsSer = {dog,cat,lemur};
+
+        ByteArrayOutputStream byteMass = new ByteArrayOutputStream();
+        ObjectOutputStream oos = new ObjectOutputStream(byteMass);
+
+        oos.writeInt(size);
+
+        for (Animal item : animalsSer)
+        {
+            oos.writeObject(item);
+        }
+
+        oos.flush();
+
+        byte[] res = new byte[size];
+        return oos.write(res);
+
+    }
     @Override
     public boolean equals(Object obj) {
         if (obj instanceof Animal) {
@@ -17,8 +47,32 @@ class Animal implements Serializable {
         }
         return false;
     }
-    public static Animal[] deserializeAnimalArray(byte[] data) {
-        // your implementation here
+    public static Animal[] deserializeAnimalArray(byte[] data) throws IOException, ClassNotFoundException {
+        Animal[] animalsDeser = new Animal[data.length];
+
+        try(ObjectInputStream in = new ObjectInputStream(new ByteArrayInputStream(data))) {
+
+            int size = in.readInt();
+
+            if(size >= 0) {
+                for (int i = 0; i < size; i++) {
+                    animalsDeser = (Animal[]) in.readObject();
+                }
+            }
+
+        } catch (IOException e){
+
+            throw new IllegalArgumentException(e);
+
+        }catch (ClassNotFoundException e){
+
+            throw new IllegalArgumentException(e);
+        }
+
+        return animalsDeser;
+
     }
+
+
 
 }
