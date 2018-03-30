20eba5fdb342a5204f4514b8c7773071e0961cfe
harly <harly1@yandex.ru>
2018-03-30 17:10:09 +0300

    LinkedList

---
+++ src/myLinkedList/Main.java
@@ -0,0 +1,14 @@
+package myLinkedList;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        List<String> list = new LinkedList<String>();
+        list.add("0");
+    }
+
+}
--- src/myLinkedList/MyLinkedList.java
+++ src/myLinkedList/MyLinkedList.java
@@ -1,10 +1,47 @@
 package myLinkedList;
 
 import java.util.LinkedList;
+import java.util.List;
 
 public class MyLinkedList<E> {
     int size = 0;
-    LinkedList<Integer> l = new LinkedList<>();
+
+    Entry<E>[] header;
+    Entry<E> first;
+    Entry<E> last;
+
+
+    public MyLinkedList(){
+        Entry<E> o = new Entry<E>(null,null,null);
+        header[0] = o;
+    }
+
+    private static class Entry<E>
+    {
+        E element;
+        Entry<E> next;
+        Entry<E> prev;
+
+        Entry(E element, Entry<E> next, Entry<E> prev)
+        {
+            this.element = element;
+            this.next = next;
+            this.prev = prev;
+        }
+    }
+
+    public void add(E element){
+
+            header= new Entry(element, header.next, header.prev);
+                    Integer.
+
+    }
+
+
+    public E get(int index) {
+        return node(index).item;
+    }
+
 
 
 
